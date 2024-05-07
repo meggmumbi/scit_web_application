fa79feae48736e3b95888eec6c7550c574ebefbf
meggmumbi <margaretmumbi16@gmail.com>
2024-05-07 22:49:31 +0300

    Admin, signin and signup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
+import {BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import SignUp from './modules/signup/SignUp';
+import SignIn from './modules/SignIn/SignIn';
+import LandingPage from './modules/landingPage/LandingPage';
+import Dashboard from './modules/dashboard/Dashboard';
+import Programmes from './modules/Programmes/Pricing';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      
+     <div className="App">
+   
+          <Routes>
+            <Route exact path="/" element={<LandingPage />} />
+            <Route path="/dashboard" element={<Dashboard />} />   
+            <Route path="/pricing" element={<Programmes />} />        
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/sign-up" element={<SignUp />} />
+
+          </Routes>
+        
+    </div> 
+  </Router>
   );
 }
 
